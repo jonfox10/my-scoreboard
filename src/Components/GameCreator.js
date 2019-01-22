4f07153b05406de9505b3ade82fa63b3240e9cc5
Jon Fuchs <jonfox@Jons-MacBook-Pro.local>
2019-01-22 15:18:13 -0700

    got game clock functionality to work properly

--- src/Components/GameCreator.js
+++ src/Components/GameCreator.js
@@ -3,20 +3,36 @@ import React, { Component } from 'react'
 import './GameCreator.css'
 import Scoreboard from './Scoreboard'
 
-class App extends Component {
+class GameCreator extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            teamOne: '',
-            teamTwo: '',
-            periodMinutes: '',
+            teamOne: 'HOME',
+            teamTwo: 'AWAY',
+            minutes: 0,
             seconds: '00',
-            visible: true,
-            clockCounting: false
+            visible: false,
+            clockStarted: false,
+            clockStopped: true,
+            clockRunning: false,
+            periodMinutes: ''
         }
-        // this.secondsRemaining;
-        // this.intervalHandle;
+        
     }
+
+    handleStartGame = (event) => {
+        this.setState({
+            visible: true,
+            minutes: this.state.periodMinutes
+        })
+    }
+
+    closeScoreboard = (event) => {
+        this.setState({
+            visible: false
+        })
+    }
+    
 
     handleInputChange = (event) => {
         this.setState({[event.target.name]: event.target.value});
@@ -26,50 +42,81 @@ class App extends Component {
         this.setState({
             teamOne: '',
             teamTwo: '',
-            periodMinutes: '',
-            seconds: '00'
+            minutes: 0,
+            seconds: 0
         })
     }
+
 
     tick = () => {
         var min = Math.floor(this.secondsRemaining / 60);
         var sec = this.secondsRemaining - (min * 60);
 
         this.setState({
-            periodMinutes: min,
+            minutes: min,
             seconds: sec
         })
-
+        
         if (sec < 10) {
             this.setState({
                 seconds: "0" + this.state.seconds,
             })
         }
-        if (min < 10) {
-            this.setState({
-                value: "0" + min,
-            })
-        }
-
+        // if (min < 10) {
+        //     this.setState({
+        //         minutes: "0" + min,
+        //     })
+        // }
+        
         if (min === 0 & sec === 0) {
             clearInterval(this.intervalHandle);
         }
-
+        
         this.secondsRemaining--
+        
     }
-    
+
     startCountDown = () => {
         this.intervalHandle = setInterval(this.tick, 1000);
-
-        let time = this.state.periodMinutes;
-
+        let time = this.state.minutes;
         this.secondsRemaining = time * 60;
-
+        this.setState({
+            clockStarted: true, clockStopped: false
+        })
+    }
+    contCountDown = () => {
+        this.intervalHandle = setInterval(this.tick, 1000);
+        this.setState({
+            clockStopped: false, clockRunning: true
+        })
     }
 
-    handleStartGame = (event) => {
-        this.setState({visible: true})
+    handleClockStop = () => {
+        clearInterval(this.intervalHandle);
+        this.setState({
+            clockStopped: true,
+            clockRunning: false
+            
+        })
     }
+    newQtr = () => {
+        if(this.state.clockStarted) {
+            if(this.state.minutes === 0) {
+                if(this.state.seconds === 0){
+                    this.setState({
+                        clockStarted: false,
+                        clockStopped: true,
+                        clockRunning: false,
+                        minutes: this.state.periodMinutes
+                    })
+                }
+            }
+            
+        }
+    }
+
+
+
 
 
 
@@ -78,6 +125,7 @@ class App extends Component {
     //   const teamTwo = this.state.teamTwo;
     return (
       <div>
+          {!this.state.visible ? (
             <div className='startForm'>
                 <h3 style={{fontSize: '6vmin', textDecoration: 'underline'}}>START GAME</h3>
                 {/* <br/> */}
@@ -119,27 +167,40 @@ class App extends Component {
                     </div>
                 </>
             </div>
+
+          ) : (
             <div>
-                { this.state.visible &&
                     <div>
                     <Scoreboard 
                         team1={this.state.teamOne}
                         team2={this.state.teamTwo}
-                        time={this.state.periodMinutes}
+                        time={this.state.minutes}
                         visible={this.state.visible}
-                        minutes={this.state.periodMinutes}
+                        minutes={this.state.minutes}
                         seconds={this.state.seconds}
                         startCountDown={this.startCountDown}
+                        startClock={this.startClock}
+                        stopClock={this.stopClock}
+                        handleClockStart={this.handleClockStart}
+                        handleClockStop={this.handleClockStop}
+                        pauseClock={this.pauseClock}
+                        contCountDown={this.contCountDown}
+                        clockStarted={this.state.clockStarted}
+                        clockStopped={this.state.clockStopped}
+                        clockRunning={this.state.clockRunning}
+                        newQtr={this.newQtr}
+                        closeScoreboard={this.closeScoreboard}
+
                         />
                     </div>
-                
-
-                }
             </div>
+
+          )
+        }
 
       </div>
     );
   }
 }
 
-export default App;
+export default GameCreator;
--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react';
 import './Scoreboard.css'
-import Countdown from 'react-countdown-now';
-
-
-
-
 
 
 
@@ -14,99 +9,135 @@ class Scoreboard extends Component {
         this.state = {
             score1: 0,
             score2: 0,
-            team1Posession: true,
+            homePossession: true,
+            awayPossession: false,
             time: this.props.time,
-        }
-
-        
-        
-        
+            period: 1
+        }      
         
     }
     addOneT1 = (event) =>  {
-        this.setState({ score1: this.state.score1 += 1})
+        this.setState({ score1: this.state.score1 + 1})
     };
     addTwoT1 = (event) =>  {
         this.setState({
-            score1: this.state.score1 += 2})
+            score1: this.state.score1 + 2})
     };
     addThreeT1 = (event) =>  {
         this.setState({
-            score1: this.state.score1 += 3})
+            score1: this.state.score1 + 3})
     };
     minusOneT1 = (event) =>  {
         this.setState({
-            score1: this.state.score1 -= 1})
-    };
-    minusTwoT1 = (event) =>  {
-        this.setState({
-            score1: this.state.score1 -= 2})
-    };
-    minusThreeT1 = (event) =>  {
-        this.setState({
-            score1: this.state.score1 -= 3})
+            score1: this.state.score1 - 1})
     };
     addOneT2 = (event, team) =>  {
         this.setState({
-            score2: this.state.score2 += 1})
+            score2: this.state.score2 + 1})
     };
     addTwoT2 = (event, team) =>  {
         this.setState({
-            score2: this.state.score2 += 2})
+            score2: this.state.score2 + 2})
     };
     addThreeT2 = (event, team) =>  {
         this.setState({
-            score2: this.state.score2 += 3})
+            score2: this.state.score2 + 3})
     };
     minusOneT2 = (event) =>  {
         this.setState({
-            score2: this.state.score2 -= 1})
-    };
-    minusTwoT2 = (event) =>  {
-        this.setState({
-            score2: this.state.score2 -= 2})
-    };
-    minusThreeT2 = (event) =>  {
-        this.setState({
-            score2: this.state.score2 -= 3})
+            score2: this.state.score2 - 1})
     };
 
-    completionist = () => <span>'00:00'</span>;
+    addPeriod = (event) => {
+        this.setState({
+            period: this.state.period + 1
+        })
+        return this.props.newQtr;
+    }
+    minusPeriod = (event) => {
+        this.setState({
+            period: this.state.period - 1
+        })
+    }
 
-    renderer = ({minutes, seconds, completed}) => {
-        if (completed) {
-            return <completionist />;
-        } else {
-            return <h1>0{minutes}:{seconds}</h1>
-        }
+    homePossessionToggle = () => {
+        this.setState({
+            homePossession: true,
+            awayPossession: false
+        })
+    }
+    awayPossessionToggle = () => {
+        this.setState({
+            homePossession: false,
+            awayPossession: true
+        })
     }
 
 
-
-    // handleSubtractScore = () => ;
   render(){
+
+    let possessionDisplay = null;
+    if (this.state.homePossession) {
+        possessionDisplay = (
+        <>
+            <h3 className='poss1' id='possYes'>POSS</h3>
+            <h3 className='poss2' id='possNo'>POSS</h3>
+        </>
+        )
+    } else if(!this.state.homePossession) {
+        possessionDisplay = (
+        <>
+            <h3 className='poss1' id='possNo'>POSS</h3>
+            <h3 className='poss2' id='possYes'>POSS</h3>
+        </>
+        )
+    }
+
+    let ctrlBtns = null;
+    if (this.props.clockStarted){  
+        ctrlBtns = (
+        <>
+            <button id='timeBtn'>start period</button>
+            <button onClick={this.props.handleClockStop} id='timeBtn'>stop</button>
+            <button onClick={this.props.contCountDown} id='timeBtn'>resume</button>
+        </>
+        )
+    } else {
+        ctrlBtns = (
+            <>
+            <button onClick={this.props.startCountDown} id='timeBtn'>start period</button>
+            <button onClick={this.props.handleClockStop} id='timeBtn'>stop</button>
+            <button onClick={this.props.contCountDown} id='timeBtn'>resume</button>
+        </>
+        )
+    }
+    let clock = null;
+    if (this.props.visiable & this.props.minutes > 9){
+        clock = (
+            <h1 className='clock'>{this.props.minutes}:{this.props.seconds}</h1>
+        )
+    } else {
+        clock = (
+            <h1 className='clock'>0{this.props.minutes}:{this.props.seconds}</h1>
+        )
+    }
+
+    
+
     return (
         <div>
+            {/*scoreboard display*/}
             <div className='scoreboardBody'>
                 <div className='scoreboardContainer'>
-                      {/* <h1 className='clock'> {this.props.minutes}:{this.props.seconds}</h1> */}
-                        <Countdown
-                        date={Date.now() + 500000}
-                        renderer={this.renderer}
-                        className='clock'
-                        autoStart='false'
-                        controlled='false'
-
-                        />
-                        <h3 className='poss1'>POSS</h3>
-                        <h3 className='poss2'>POSS</h3>
+                        {clock}
+                        {possessionDisplay} 
                       <div className='team1'>
                         <h3 id='team1'>{this.props.team1}</h3>
                         <h1>{this.state.score1}</h1>
                       </div>
                       <div className='per'>
                         <p>PER:</p>
-                        <h1>ot</h1>
+                        <h1>{this.state.period}</h1>
                       </div>
                       <div className='team2'>
                         <h3 id='team2'>{this.props.team2}</h3>
@@ -114,42 +145,38 @@ class Scoreboard extends Component {
                       </div>
                 </div>
             </div>
+            {/* scoreboard control panel */}
             <div className='ctrls'>
                 <div className='timeCtrl'>
-                    TIME: 
-                    <button onClick={this.props.startCountDown} id='timeBtn'>start</button>
-                    <button id='timeBtn'>stop</button>
-                    <input type='time'></input>
-                    <button id='timeBtn'>reset</button>
-                    P:<input type='number'></input>
+                    <p>time:</p>
+                    {ctrlBtns}
+                    {/* <input type='time'></input> */}
+                    <button onClick={this.props.newQtr} id='timeBtn'>reset</button>
+                    p: <button className='perBtn' onClick={this.addPeriod}>+</button>
+                    <button className='perBtn' onClick={this.minusPeriod}>-</button>
                 </div>
                 <div className='team1Ctrl'>
-                    TEAM 1
+                {this.props.team1}
+                <button className='possBtn' onClick={this.homePossessionToggle}>possession</button>
                     <div>
                         <button id='scoreBtnAdd' onClick={this.addOneT1}>+1</button>
                         <button id='scoreBtnAdd' onClick={this.addTwoT1}>+2</button>
                         <button id='scoreBtnAdd' onClick={this.addThreeT1}>+3</button>
-                    </div>
-                    <div>
                         <button id='scoreBtnMinus' onClick={this.minusOneT1}>-1</button>
-                        <button id='scoreBtnMinus' onClick={this.minusTwoT1}>-2</button>
-                        <button id='scoreBtnMinus' onClick={this.minusThreeT1}>-3</button>
                     </div>
                 </div>
                 <div className='team2Ctrl'>
-                    TEAM 2
+                {this.props.team2}
+                <button className='possBtn' onClick={this.awayPossessionToggle}>possession</button>
                     <div>
                         <button id='scoreBtnAdd' onClick={this.addOneT2}>+1</button>
                         <button id='scoreBtnAdd' onClick={this.addTwoT2}>+2</button>
                         <button id='scoreBtnAdd' onClick={this.addThreeT2}>+3</button>
-                    </div>
-                    <div>
                         <button id='scoreBtnMinus' onClick={this.minusOneT2}>-1</button>
-                        <button id='scoreBtnMinus' onClick={this.minusTwoT2}>-2</button>
-                        <button id='scoreBtnMinus' onClick={this.minusThreeT2}>-3</button>
                     </div>
                 </div>
             </div>
+                <button id='timeBtn' onClick={this.props.closeScoreboard}>close scoreboard</button>
                 
         </div>
     )
