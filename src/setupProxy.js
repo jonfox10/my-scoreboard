5d6c97fb150666acb05248f819df434f10ee6e5e
Jon Fox <jonfuchs45@gmail.com>
2019-03-26 10:25:46 -0600

    trying to settle the merge conflicts

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,4 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
   app.use(proxy('/api', { target: 'http://localhost:4100/' }))
   app.use(proxy('/auth', { target: 'http://localhost:4100/' }))
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6d83a8e7ed3d4d4afcdb06ed6a2ce778de462748
